f34f4ceba66159e89bc5bce4a1e6d5bc1e1bff29
hurzelpurzel <ludger.pottmeier@gmx.de>
2017-02-10 23:04:05 +0100

    Persistenz funktioniert

--- src/main/java/de/pottmeier/beans/TerminRepository.java
+++ src/main/java/de/pottmeier/beans/TerminRepository.java
@@ -16,6 +16,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -23,64 +24,64 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class TerminRepository {
-    
-    private static final String PERSISTENCE_UNIT_NAME = "knobelapp_pu";
-   
-    
-    
-    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
-    EntityManager em;
-    
 
+    @PersistenceContext()
+    EntityManager em;
+
+    @Transactional
     public TerminDto create(TerminDto in) {
         TerminPO po = new TerminPO();
         BeanUtils.copyProperties(in, po);
+
         em.persist(po);
-        
+
         return wrap(po);
     }
-    
-     public boolean change(TerminDto in){
-         TerminPO po = em.find(TerminPO.class, in.getId());
-         if(po != null){
-             
-            BeanUtils.copyProperties(in, po);
-             em.merge(po);
-             
-             return true;
-         }
-         return false;
-     }
-    
-    public Set<TerminDto> findAll(){
-        Query q=em.createQuery("select * from termin");
-        return  wrapAll((Set<TerminPO>)q.getResultList().stream().collect(Collectors.toSet()));
-        
-    }
-    
-    public boolean delete(TerminDto in){
+
+    @Transactional
+    public boolean change(TerminDto in) {
         TerminPO po = em.find(TerminPO.class, in.getId());
-        if(po != null){
-             em.remove(po);
-             return true;
+        if (po != null) {
+
+            BeanUtils.copyProperties(in, po);
+
+            em.merge(po);
+
+            return true;
         }
-         return false;
-     }
-    
-    private static TerminDto wrap(TerminPO po){
-        TerminDto res =new TerminDto();
+        return false;
+    }
+
+    public Set<TerminDto> findAll() {
+        Query q = em.createQuery("select t from " + TerminPO.class.getSimpleName() + " t");
+        return wrapAll((Set<TerminPO>) q.getResultList().stream().collect(Collectors.toSet()));
+
+    }
+
+    @Transactional
+    public boolean delete(TerminDto in) {
+        TerminPO po = em.find(TerminPO.class, in.getId());
+        if (po != null) {
+
+            em.remove(po);
+
+            return true;
+        }
+        return false;
+    }
+
+    private static TerminDto wrap(TerminPO po) {
+        TerminDto res = new TerminDto();
         res.setId(po.getId());
         res.setOrt(po.getOrt());
         res.setAnlass(po.getAnlass());
         res.setTermin(po.getTermin());
         return res;
     }
-    
-    private static Set<TerminDto> wrapAll(Set<TerminPO> in){
-        
+
+    private static Set<TerminDto> wrapAll(Set<TerminPO> in) {
+
         return in.stream().map((TerminPO po) -> wrap(po)).collect(Collectors.toSet());
     }
-    
-    
-    
+
 }
--- src/main/java/de/pottmeier/model/TerminPO.java
+++ src/main/java/de/pottmeier/model/TerminPO.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
  *
  * @author ludger
  */
-@Entity(name = "termin")
+@Entity()
 public class TerminPO implements Serializable {
 
     private static final long serialVersionUID = 1L;
